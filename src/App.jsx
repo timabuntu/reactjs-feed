ecf428bd16336622e3041de21e92a5823eb9fade
thiago lopes de mello <timabuntu@gmail.com>
2022-06-14 09:32:29 -0300

    App styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { wrapper } from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <div className='App'>
       <Header />
-      <Post />
+      <div className={wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
   );
 }
