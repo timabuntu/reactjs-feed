e1fe2e4618da93eb32084a906170d2a9463545b0
thiago lopes de mello <timabuntu@gmail.com>
2022-06-15 11:13:44 -0300

    style: text profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,10 @@ export function Sidebar() {
           src='https://images.unsplash.com/photo-1642697283420-194938fcc339?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
         />
         <div className={styles.profile}>
-          <img src='https://avatars.githubusercontent.com/u/58628277?v=4' />
+          <img
+            className={styles.avatar}
+            src='https://avatars.githubusercontent.com/u/58628277?v=4'
+          />
           <strong>Thiago Mello</strong>
           <span>Web Developer</span>
         </div>
